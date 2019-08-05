bdeb59e2d0942b6335eeca655466931703a9755e
Jengopockets <jengopockets@gmail.com>
2019-08-05 18:09:24 -0400

    Post Functionallity

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // implement your API here
 const express = require('express');
 
-const dataB = require('./data/db.js');
+const DataB = require('./data/db.js');
 
 const server = express();
 
@@ -9,6 +9,47 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('hello world')
+});
+//User List Get /api/users
+server.get('/api/users', (req, res) => {
+    DataB.find()
+    .then(users => {
+        res.status(201).json(users);
+    })
+    .catch(error => {
+        res.status(500).json({message:"You Are Alone Here"})
+    });
+});
+// Get /api/users/:id
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    DataB.find(id)
+    .then(user => {
+        if (user) {
+            res.status(201).json(user);
+        }else {
+            res.status(404).json({message: 'user is imaginary'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message:"You Are Alone Here"})
+    });
+});
+//Post
+server.post('/api/users', (req, res) => {
+    const {name, bio} = req.body;
+    if (!name || !bio) {
+        return res
+        .status(400)
+        .json({message:"Name and Bio Please"});
+    }
+    DataB.insert({name, bio})
+    .then(newUser => {
+        res.status(200).json(newUser);
+    })
+    .catch(error => {
+        res.status(500).json({message:"Sorry You Didn't do it right"})
+    });
 });
 
 const port = 8000;
